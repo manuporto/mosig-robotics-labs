615d8e3f8a82362d0d4df28da58008117f9ad5f2
Pinkhat <mahdis.ghaffarian@gmail.com>
2019-04-16 16:50:22 +0200

    globalplanner updated

--- src/surveillance_robot/src/global_planner_node.cpp
+++ src/surveillance_robot/src/global_planner_node.cpp
@@ -12,7 +12,7 @@
 #include <tf/transform_datatypes.h>
 
 // Number of vertices in the graph
-#define V 6
+#define V 20
 
 class global_planner {
 private:
@@ -34,9 +34,32 @@ private:
   geometry_msgs::Point p2;
   geometry_msgs::Point p3;
   geometry_msgs::Point p4;
+  geometry_msgs::Point p5;
+  geometry_msgs::Point p6;
+  geometry_msgs::Point p7;
+  geometry_msgs::Point p8;
+  geometry_msgs::Point p9;
+  geometry_msgs::Point p10;
+  geometry_msgs::Point p11;
+  geometry_msgs::Point p12;
+  geometry_msgs::Point p13;
+  geometry_msgs::Point p14;
+  geometry_msgs::Point p15;
+  geometry_msgs::Point p16;
+  geometry_msgs::Point p17;
+  geometry_msgs::Point p18;
+  geometry_msgs::Point p19;
+  geometry_msgs::Point p20;
+  geometry_msgs::Point p21;
+
+  geometry_msgs::Point pointArray[v] = {p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18,p19,p20,p21};
+  geometry_msgs::Point finalPathPoint[v];
 
   int state;
   bool display_state;
+
+  int src = ;
+  int goal = ;
 
 public:
   global_planner() {
@@ -48,12 +71,66 @@ public:
     // Set up point coordinates
     p1.x = 17.878;
     p1.y = -22.837;
-    p2.x = 13.603;
-    p2.y = -8.464;
-    p3.x = 9.795;
-    p3.y = 6.990;
-    p4.x = 26.651;
-    p4.y = -4.985;
+
+    p2.x = 17.878;
+    p2.y = -20.837;
+
+    p3.x = 17.878;
+    p3.y = -18.837;
+
+    p4.x = 17.878;
+    p4.y = -16.837;
+
+    p5.x = 17.878;
+    p5.y = -14.837;
+
+    p6.x = 17.878;
+    p6.y = -12.837;
+
+    p7.x = 17.878;
+    p7.y = -10.837;        
+
+    p8.x = 13.603;
+    p8.y = -8.464;
+
+    p9.x = 13.603;
+    p9.y = -6.464;
+
+    p10.x = 13.603;
+    p10.y = -4.464;
+
+    p11.x = 13.603;
+    p11.y = -2.464;
+
+    p12.x = 13.603;
+    p12.y = 0.464;
+
+    p13.x = 13.603;
+    p13.y = 2.464;
+
+    p14.x = 13.603;
+    p14.y = 4.464;    
+
+    p15.x = 13.603;
+    p15.y = 6.990;
+
+    p16.x = 15.603;
+    p16.y = -4.985;
+
+    p17.x = 17.603;
+    p17.y = -4.985;
+
+    p18.x = 19.603;
+    p18.y = -4.985;
+
+    p19.x = 21.603;
+    p19.y = -4.985; 
+
+    p20.x = 23.603;
+    p20.y = -4.985;                
+
+    p21.x = 26.651;
+    p21.y = -4.985;
 
     // INFINTE LOOP TO COLLECT POSITION DATA AND PROCESS THEM
     ros::Rate r(10); // this node will work at 10hz
@@ -65,45 +142,21 @@ public:
     }
   }
 
+  int graph[v][v];
   void estimate_graph_position() {
 
-    float d12 = distancePoints(p1, p2);
-    float d23 = distancePoints(p2, p3);
-    float d24 = distancePoints(p2, p4);
+      for(int i=0; i<v; i++)
+        for(int j=0; j<v; j++)
+        {
+            if( abs(i-j) == 1)
+                graph[i][j] = 1;
+            else
+                graph[i][j] = 0;      
+        }
 
-    if (current_position_estim.position.y < p3.y &&
-            current_position_estim.position.y > p2.y && goal_to_reach.x < p4.x &&
-            goal_to_reach.x > p2.x ||
-            goal_to_reach.y < p3.y && goal_to_reach.y > p2.y &&
-            current_position_estim.x < p4.x &&
-            current_position_estim.x > p2.x) { // FIXME
-      float d3c = distancePoints(p3, current_position_estim.position);
-      float d2c = distancePoints(p2, current_position_estim.position);
-      float d2g = distancePoints(p2, goal_to_reach);
-      float d4g = distancePoints(p4, goal_to_reach);
-
-      float graph[6][6] = {{0, d12, 0, 0, 0, 0},   {d12, 0, 0, 0, d2c, d2g},
-                           {0, 0, 0, 0, d3c, 0},   {0, 0, 0, 0, 0, d4g},
-                           {0, d2c, d3c, 0, 0, 0}, {0, d2g, 0, d4g, 0, 0}};
-      dijkstra(graph, current_position_estim);
+      dijkstra(graph, src);
     }
 
-    if (current_position_estim.position.y < p2.y &&
-            current_position_estim.position.y > p1.y && goal_to_reach.x < p4.x &&
-            goal_to_reach.x > p2.x ||
-            goal_to_reach.y < p2.y && goal_to_reach.y > p1.y &&
-            current_position_estim.positionx < p4.x &&
-            current_position_estim.position.x > p2.x) { // FIXME
-      float d1c = distancePoints(p1, current_position_estim.position);
-      float d2c = distancePoints(p2, current_position_estim.position);
-      float d2g = distancePoints(p2, goal_to_reach);
-      float d4g = distancePoints(p4, goal_to_reach);
-
-      float graph[6][6] = {{0, 0, 0, 0, d1c, 0},   {0, 0, d23, 0, d2c, d2g},
-                           {0, d23, 0, 0, 0, 0},   {0, 0, 0, 0, 0, d4g},
-                           {d1c, d2c, 0, 0, 0, 0}, {0, d2g, 0, d4g, 0, 0}};
-      dijkstra(graph, current_position_estim);
-    }
   }
 
   // UPDATE: main processing
@@ -144,16 +197,8 @@ public:
     return min_index;
   }
 
-  /* // A utility function to print the constructed distance array
-  int printSolution(int dist[], int n)
-  {
-      printf("Vertex Distance from Source\n");
-      for (int i = 0; i < V; i++)
-          printf("%d tt %d\n", i, dist[i]);
-  }  */
-
   // Function to print shortest
-  // path from source to j
+  // path from source to goal
   // using path array
   void printPath(int path[], int j) {
     // Base Case : If j is source
@@ -161,29 +206,43 @@ public:
       return;
 
     printPath(path, path[j]);
+
+    int finalpath[v];
+    for(int i=0; i<v; i++){
+        finalpath[i] = j;
+    }
+    int lenght= size(finalpath);
+    finalPathPoint(finalpath,lenght);
   }
+
+  void finalPathPoint(int arr[], int l){
+
+      for(int i=0; i<l; i++){
+        finalPathPoint[i]= arr[i];
+      }
+  }
+
 
   // A utility function to print
   // the constructed distance
   // array
-  int printSolution(int dist[], int n, int path[]) {
+/*   int printSolution(int dist[], int n, int path[]) {
     int src = 0;
     printf("Vertex\t Distance\tPath");
     for (int i = 1; i < V; i++) {
       printf("\n%d -> %d \t\t %d\t\t%d ", src, i, dist[i], src);
       printPath(path, i);
     }
-  }
+  } */
 
   // Function that implements Dijkstra's single source shortest path algorithm
   // for a graph represented using adjacency matrix representation
-  void dijkstra(float graph[V][V], int currentposition) {
+  void dijkstra(int graph[V][V], int src) {
     int dist[V]; // The output array.  dist[i] will hold the shortest
-    // distance from currentposition to i
+    // distance from src to i
 
-    bool
-        sptSet[V]; // sptSet[i] will be true if vertex i is included in shortest
-    // path tree or shortest distance from currentposition to i is finalized
+    bool sptSet[V]; // sptSet[i] will be true if vertex i is included in shortest
+    // path tree or shortest distance from src to i is finalized
 
     // path array to store
     // shortest path tree
@@ -198,12 +257,12 @@ public:
     }
 
     // Distance of source vertex from itself is always 0
-    dist[currentposition] = 0;
+    dist[src] = 0;
 
     // Find shortest path for all vertices
     for (int count = 0; count < V - 1; count++) {
       // Pick the minimum distance vertex from the set of vertices not
-      // yet processed. u is always equal to currentposition in the first
+      // yet processed. u is always equal to src in the first
       // iteration.
       int u = minDistance(dist, sptSet);
 
@@ -226,7 +285,7 @@ public:
     }
 
     // print the constructed distance array
-    // printSolution(dist, V);
+    printPath(path, goal);  
   }
 
   // Distance between two points
