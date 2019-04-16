da1612c130326dcf5d57a921524343b573b7e90a
Manuel Porto <manu.porto94@gmail.com>
2019-04-16 17:19:09 +0200

    Fix syntax errors

--- src/surveillance_robot/src/global_planner_node.cpp
+++ src/surveillance_robot/src/global_planner_node.cpp
@@ -1,5 +1,5 @@
-#include "geometry_msgs/PoseArray.h"
 #include "geometry_msgs/Point.h"
+#include "geometry_msgs/PoseArray.h"
 #include "geometry_msgs/PoseWithCovarianceStamped.h"
 #include "geometry_msgs/Quaternion.h"
 #include "nav_msgs/Odometry.h"
@@ -53,14 +53,16 @@ private:
   geometry_msgs::Point p20;
   geometry_msgs::Point p21;
 
-  geometry_msgs::Point pointArray[v] = {p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18,p19,p20,p21};
-  geometry_msgs::Point finalPathPoint[v];
+  geometry_msgs::Point pointArray[V] = {p1,  p2,  p3,  p4,  p5,  p6,  p7,
+                                        p8,  p9,  p10, p11, p12, p13, p14,
+                                        p15, p16, p17, p18, p19, p20, p21};
+  geometry_msgs::Point finalPathPoint[V];
 
   int state;
   bool display_state;
 
-  int src = ;
-  int goal = ;
+  int src;
+  int goal;
 
 public:
   global_planner() {
@@ -89,7 +91,7 @@ public:
     p6.y = -12.837;
 
     p7.x = 17.878;
-    p7.y = -10.837;        
+    p7.y = -10.837;
 
     p8.x = 13.603;
     p8.y = -8.464;
@@ -110,7 +112,7 @@ public:
     p13.y = 2.464;
 
     p14.x = 13.603;
-    p14.y = 4.464;    
+    p14.y = 4.464;
 
     p15.x = 13.603;
     p15.y = 6.990;
@@ -125,14 +127,16 @@ public:
     p18.y = -4.985;
 
     p19.x = 21.603;
-    p19.y = -4.985; 
+    p19.y = -4.985;
 
     p20.x = 23.603;
-    p20.y = -4.985;                
+    p20.y = -4.985;
 
     p21.x = 26.651;
     p21.y = -4.985;
 
+    src = 1;
+    goal = 1;
     // INFINTE LOOP TO COLLECT POSITION DATA AND PROCESS THEM
     ros::Rate r(10); // this node will work at 10hz
     while (ros::ok()) {
@@ -143,90 +147,85 @@ public:
     }
   }
 
-  int graph[v][v];
+  int graph[V][V];
   void estimate_graph_position() {
 
-      for(int i=0; i<v; i++)
-        for(int j=0; j<v; j++)
-        {
-            if( abs(i-j) == 1)
-                graph[i][j] = 1;
-            else
-                graph[i][j] = 0;      
-        }
-
-      dijkstra(graph, src);
-    }
-
-  }
-
-  // UPDATE: main processing
-  /*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
-  void update() {
-    if (!display_state) {
-      display_state = true;
-      ROS_INFO("state: %i", state);
-    }
-
-    // we receive a new /goal_to_reach and robair is not doing a translation or
-    // a rotation
-    if (new_goal_to_reach) {
-      // calculation
-      // publish
-      ROS_INFO("publishing new goal to reach");
-      pub_goal_to_reach.publish(goal_to_reach);
-      new_goal_to_reach = false;
-    }
-
-    if (new_pose) {
-      estimate_graph_position();
-    }
-
-  } // update
-
-  // A utility function to find the vertex with minimum distance value, from
-  // the set of vertices not yet included in shortest path tree
-  int minDistance(int dist[], bool sptSet[]) {
-    // Initialize min value
-    int min = INT_MAX, min_index;
-
-    for (int v = 0; v < V; v++)
-      if (sptSet[v] == false && dist[v] <= min)
-        min = dist[v], min_index = v;
-
-    return min_index;
-  }
-
-  // Function to print shortest
-  // path from source to goal
-  // using path array
-  void printPath(int path[], int j) {
-    // Base Case : If j is source
-    if (path[j] == -1)
-      return;
-
-    printPath(path, path[j]);
-
-    int finalpath[v];
-    for(int i=0; i<v; i++){
-        finalpath[i] = j;
-    }
-    int lenght= size(finalpath);
-    finalPathPoint(finalpath,lenght);
-  }
-
-  void finalPathPoint(int arr[], int l){
-
-      for(int i=0; i<l; i++){
-        finalPathPoint[i]= arr[i];
+    for (int i = 0; i < V; i++)
+      for (int j = 0; j < V; j++) {
+        if (abs(i - j) == 1)
+          graph[i][j] = 1;
+        else
+          graph[i][j] = 0;
       }
+
+    dijkstra(graph, src);
   }
 
+// UPDATE: main processing
+/*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+void update() {
+  if (!display_state) {
+    display_state = true;
+    ROS_INFO("state: %i", state);
+  }
 
-  // A utility function to print
-  // the constructed distance
-  // array
+  // we receive a new /goal_to_reach and robair is not doing a translation or
+  // a rotation
+  if (new_goal_to_reach) {
+    // calculation
+    // publish
+    ROS_INFO("publishing new goal to reach");
+    pub_goal_to_reach.publish(goal_to_reach);
+    new_goal_to_reach = false;
+  }
+
+  if (new_pose) {
+    estimate_graph_position();
+  }
+
+} // update
+
+// A utility function to find the vertex with minimum distance value, from
+// the set of vertices not yet included in shortest path tree
+int minDistance(int dist[], bool sptSet[]) {
+  // Initialize min value
+  int min = INT_MAX, min_index;
+
+  for (int i = 0; i < V; i++)
+    if (sptSet[i] == false && dist[i] <= min)
+      min = dist[i], min_index = i;
+
+  return min_index;
+}
+
+void getFinalPathPoint(int arr[], int l) {
+  for (int i = 0; i < l; i++) {
+    finalPathPoint[i] = pointArray[arr[i]];
+  }
+}
+
+// Function to print shortest
+// path from source to goal
+// using path array
+void printPath(int path[], int j) {
+  // Base Case : If j is source
+  if (path[j] == -1)
+    return;
+
+  printPath(path, path[j]);
+
+  int finalpath[V];
+  for (int i = 0; i < V; i++) {
+    finalpath[i] = j;
+  }
+  int lenght = V; // FIXME
+  getFinalPathPoint(finalpath, lenght);
+}
+
+// A utility function to print
+// the constructed distance
+// array
 /*   int printSolution(int dist[], int n, int path[]) {
     int src = 0;
     printf("Vertex\t Distance\tPath");
@@ -236,97 +235,99 @@ public:
     }
   } */
 
-  // Function that implements Dijkstra's single source shortest path algorithm
-  // for a graph represented using adjacency matrix representation
-  void dijkstra(int graph[V][V], int src) {
-    int dist[V]; // The output array.  dist[i] will hold the shortest
-    // distance from src to i
+// Function that implements Dijkstra's single source shortest path algorithm
+// for a graph represented using adjacency matrix representation
+void dijkstra(int graph[V][V], int src) {
+  int dist[V]; // The output array.  dist[i] will hold the shortest
+  // distance from src to i
 
-    bool sptSet[V]; // sptSet[i] will be true if vertex i is included in shortest
-    // path tree or shortest distance from src to i is finalized
+  bool sptSet[V]; // sptSet[i] will be true if vertex i is included in shortest
+  // path tree or shortest distance from src to i is finalized
 
-    // path array to store
-    // shortest path tree
-    int path[V];
+  // path array to store
+  // shortest path tree
+  int path[V];
 
-    // Initialize all distances as
-    // INFINITE and stpSet[] as false
-    for (int i = 0; i < V; i++) {
-      path[0] = -1;
-      dist[i] = INT_MAX;
-      sptSet[i] = false;
-    }
-
-    // Distance of source vertex from itself is always 0
-    dist[src] = 0;
-
-    // Find shortest path for all vertices
-    for (int count = 0; count < V - 1; count++) {
-      // Pick the minimum distance vertex from the set of vertices not
-      // yet processed. u is always equal to src in the first
-      // iteration.
-      int u = minDistance(dist, sptSet);
-
-      // Mark the picked vertex as processed
-      sptSet[u] = true;
-
-      // Update dist value of the adjacent vertices of the picked vertex.
-      for (int v = 0; v < V; v++)
-        // Update dist[v] only if is
-        // not in sptSet, there is
-        // an edge from u to v, and
-        // total weight of path from
-        // src to v through u is smaller
-        // than current value of
-        // dist[v]
-        if (!sptSet[v] && graph[u][v] && dist[u] + graph[u][v] < dist[v]) {
-          path[v] = u;
-          dist[v] = dist[u] + graph[u][v];
-        }
-    }
-
-    // print the constructed distance array
-    printPath(path, goal);  
+  // Initialize all distances as
+  // INFINITE and stpSet[] as false
+  for (int i = 0; i < V; i++) {
+    path[0] = -1;
+    dist[i] = INT_MAX;
+    sptSet[i] = false;
   }
 
-  geometry_msgs::Pose get_nearest_node(const geometry_msgs::PoseArray::ConstPtr &nodes, const geometry_msgs::Pose::ConstPtr &current_node) {
-    float min_distance = std::numeric_limits<float>::max();
-    geometry_msgs::Pose nearest_node;
-    for (geometry_msgs::Pose node : nodes->poses) {
-      if (distancePoints(node.position, current_node->position) < min_distance) {
-        nearest_node.position.x = node.position.x;
-        nearest_node.position.y = node.position.y;
+  // Distance of source vertex from itself is always 0
+  dist[src] = 0;
+
+  // Find shortest path for all vertices
+  for (int count = 0; count < V - 1; count++) {
+    // Pick the minimum distance vertex from the set of vertices not
+    // yet processed. u is always equal to src in the first
+    // iteration.
+    int u = minDistance(dist, sptSet);
+
+    // Mark the picked vertex as processed
+    sptSet[u] = true;
+
+    // Update dist value of the adjacent vertices of the picked vertex.
+    for (int i = 0; i < V; i++)
+      // Update dist[V] only if is
+      // not in sptSet, there is
+      // an edge from u to V, and
+      // total weight of path from
+      // src to V through u is smaller
+      // than current value of
+      // dist[V]
+      if (!sptSet[i] && graph[u][i] && dist[u] + graph[u][i] < dist[i]) {
+        path[i] = u;
+        dist[i] = dist[u] + graph[u][i];
       }
+  }
+
+  // print the constructed distance array
+  printPath(path, goal);
+}
+
+geometry_msgs::Pose
+get_nearest_node(const geometry_msgs::PoseArray::ConstPtr &nodes,
+                 const geometry_msgs::Pose::ConstPtr &current_node) {
+  float min_distance = std::numeric_limits<float>::max();
+  geometry_msgs::Pose nearest_node;
+  for (geometry_msgs::Pose node : nodes->poses) {
+    if (distancePoints(node.position, current_node->position) < min_distance) {
+      nearest_node.position.x = node.position.x;
+      nearest_node.position.y = node.position.y;
     }
-    return nearest_node;
   }
+  return nearest_node;
+}
 
-  // Distance between two points
-  float distancePoints(geometry_msgs::Point pa, geometry_msgs::Point pb) {
+// Distance between two points
+float distancePoints(geometry_msgs::Point pa, geometry_msgs::Point pb) {
 
-    return sqrt(pow((pa.x - pb.x), 2.0) + pow((pa.y - pb.y), 2.0));
-  }
+  return sqrt(pow((pa.x - pb.x), 2.0) + pow((pa.y - pb.y), 2.0));
+}
 
-  // CALLBACKS
-  /*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
-  void
-  getPosition(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr &msg) {
-    ROS_INFO_STREAM("Estimated position: " << msg);
-    current_position_estim.position = msg->pose.pose.position;
-    current_position_estim.orientation = msg->pose.pose.orientation;
-    ROS_INFO_STREAM(
-        "Saved as current_position_estim :" << current_position_estim << "\n");
-    new_pose = true;
-  }
+// CALLBACKS
+/*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+void getPosition(
+    const geometry_msgs::PoseWithCovarianceStamped::ConstPtr &msg) {
+  ROS_INFO_STREAM("Estimated position: " << msg);
+  current_position_estim.position = msg->pose.pose.position;
+  current_position_estim.orientation = msg->pose.pose.orientation;
+  ROS_INFO_STREAM("Saved as current_position_estim :" << current_position_estim
+                                                      << "\n");
+  new_pose = true;
+}
 
-  void getPointGoal(const geometry_msgs::PoseStamped::ConstPtr &msg) {
-    ROS_INFO_STREAM("Received final pose: " << msg);
-    goal_to_reach.x = msg->pose.position.x;
-    goal_to_reach.y = msg->pose.position.y;
-    ROS_INFO_STREAM("Saved as goal_to_reach :" << goal_to_reach);
-    new_goal_to_reach = true;
-  }
+void getPointGoal(const geometry_msgs::PoseStamped::ConstPtr &msg) {
+  ROS_INFO_STREAM("Received final pose: " << msg);
+  goal_to_reach.x = msg->pose.position.x;
+  goal_to_reach.y = msg->pose.position.y;
+  ROS_INFO_STREAM("Saved as goal_to_reach :" << goal_to_reach);
+  new_goal_to_reach = true;
+}
 };
 
 int main(int argc, char **argv) {
